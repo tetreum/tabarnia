fb0cf4adde53268df75c97428efea1aa01eed392
muertet <fp557s@gmail.com>
2019-03-24 17:51:25 +0100

    + More building models + Metro entrance model [WIP] + Traffic system

---
+++ Assets/Scripts/Pedestrian/Pedestrian.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pedestrian : MonoBehaviour {
+
+    Animator anim;
+
+    private void Awake() {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Start() {
+        anim.SetFloat("Forward", 0.5f);
+    }
+}
